7fc5459ef96006b908f0a453bcdb172e23864911
ROZYTYAGI <tyagirozy4@gmail.com>
2017-07-05 16:31:59 +0530

    Add files via upload

---
+++ StrComp.java
@@ -0,0 +1,12 @@
+class StrComp
+{
+public static void main(String a[])
+{
+String str1="abc";
+String str2="abc";
+String str3=new String("abc");
+System.out.println(str1==str2);
+System.out.println(str2==str3);
+System.out.println(str1==str3);
+}
+}
